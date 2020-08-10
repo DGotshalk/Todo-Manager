fc71a8784ef731a822148dc608c625dab221ff10
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2020-08-09 22:46:58 -1000

    made my list class ToDo: 	make list items checkable

---
+++ Todo/daily_items.cpp
@@ -0,0 +1,6 @@
+#include "daily_items.h"
+
+Daily_items::Daily_items(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ Todo/daily_items.h
@@ -0,0 +1,17 @@
+#ifndef DAILY_ITEMS_H
+#define DAILY_ITEMS_H
+
+#include <QWidget>
+
+class Daily_items : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Daily_items(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // DAILY_ITEMS_H
