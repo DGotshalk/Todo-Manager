c3c13afbea3c31f19aaf4613a0fdd83eb4ea1259
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2020-08-09 18:59:37 -1000

    started making todo list

---
+++ Todo/dailyitems.cpp
@@ -0,0 +1,6 @@
+#include "dailyitems.h"
+
+Dailyitems::Dailyitems()
+{
+
+}
---
+++ Todo/dailyitems.h
@@ -0,0 +1,13 @@
+#ifndef DAILYITEMS_H
+#define DAILYITEMS_H
+
+#include <QObject>
+#include <QWidget>
+
+class Dailyitems
+{
+public:
+    Dailyitems();
+};
+
+#endif // DAILYITEMS_H
