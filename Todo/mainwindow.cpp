ecd98bb85cf6f1f50aac6656cc491af1f789235e
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2020-08-10 19:05:51 -1000

    added

--- Todo/mainwindow.cpp
+++ Todo/mainwindow.cpp
@@ -7,10 +7,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Daily_items ItemList(ui->daily_items);
+    Daily_items newitem;
+    newitem.push_back("memes");
+    newitem.push_back("i cant believe its not butter");
+    ui->dailylistWidget->Load(newitem);
+    QObject::connect(ui->editlistButton,SIGNAL(clicked()),
+            this, SLOT(OpenEditDialogue()));
 }
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
+void MainWindow::OpenEditDialogue()
+{
+    std::cout << "open dialogue" <<std::endl;
+}
