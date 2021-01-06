044d157871b746522f6ec71fe54d02f08d2ccf2e
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2021-01-05 23:35:54 -0800

    fixed csv writing numbers

--- Todo/weekly_items.cpp
+++ Todo/weekly_items.cpp
@@ -34,7 +34,10 @@ void Weekly_items::push_back(QString details){
 QString Weekly_items::QDate_To_Week(){
 	QString week = QString::number(cur_date.weekNumber());
 	QString year = QString::number(cur_date.year());
-	return year+"."+week;
+	if (cur_date.weekNumber() < 10)
+		return year+".0"+week;
+	else 
+		return year+"."+week;
 }
 
 
